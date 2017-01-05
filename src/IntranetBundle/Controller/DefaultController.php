9096354638864bc9cc20ffa556ae5c7a36ce4f94
goju <julienbrandin@sfr.fr>
2017-01-05 08:42:14 +0100

    FOSuser

---
+++ src/IntranetBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace IntranetBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('IntranetBundle:Default:index.html.twig');
+    }
+}
---
+++ src/IntranetBundle/IntranetBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace IntranetBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IntranetBundle extends Bundle
+{
+}
